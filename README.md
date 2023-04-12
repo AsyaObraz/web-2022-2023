afadd7e638395dc7f820e3a29c8dc88165b7c143
AsyaObraz <asyaobraz17@gmail.com>
2023-04-12 22:14:54 +0300

    5 лаба

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-2022-2023
+Obraztsova Anastasia, MK-11
+Institute iSpring
---
+++ cmd/blog/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = ":3000"
+	mux := http.NewServerMux()
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	mux.HandleFunc("/home", home)
+	mux.HandleFunc("/post", post)
+	err := http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
